95d93e65f5871abed7143b7205d515faf2fc0ef5
Chris <norish.chris@gmail.com>
2021-06-02 19:22:28 +0100

    Feat: Added location library - welcome screen now asks for location and returns an object with various location data in .json

--- Screens/WelcomeLocationModal.jsx
+++ Screens/WelcomeLocationModal.jsx
@@ -1,8 +1,27 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+// npm i react-native-location
+// LOCATION
+import * as Location from 'expo-location';
+
 const WelcomeLocationModal = ({ navigation }) => {
+  // LOCATION STATE
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  // CALL TO LOCATION API
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+
   // OnPress handler - closes modal and navs to Home page
   const handleSubmit = useCallback(() => {
     navigation.navigate('Main');
@@ -10,6 +29,13 @@ const WelcomeLocationModal = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={() => getLocation()}>
+        <Text>Touch to get location</Text>
+      </TouchableOpacity>
+      <Text>Latitude = {location ? location.coords.latitude : 'pending'}</Text>
+      <Text>
+        Longitude = {location ? location.coords.longitude : 'pending'}
+      </Text>
       <TouchableOpacity style={styles.button} onPress={() => handleSubmit()}>
         <Text>Touch to enter</Text>
       </TouchableOpacity>
